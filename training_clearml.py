13d2d2eae626bdd25f9dc7bb9e8ba07702b8c2df
Daniel Langenkämper <dlangenk@gmail.com>
2026-01-08 10:51:26 +0000

    datasets

--- training_clearml.py
+++ training_clearml.py
@@ -2,7 +2,8 @@ from rfdetr import RFDETRBase, RFDETRSmall, RFDETRMedium, RFDETRLarge
 import argparse
 import clearml
 
-def train_rf_detr(model_size='base',epochs=20,dataset_name="/mnt/fish-detection-coco-dataset/benthic_f_small/rf_data",output_uri="/mnt/fish-detection-coco-dataset/benthic_f_small/models/rf_detr10"):
+def train_rf_detr(model_size='base',epochs=20,dataset_name="/mnt/fish-detection-coco-dataset/benthic_f_small/rf_data",
+                    output_uri="/mnt/fish-detection-coco-dataset/benthic_f_small/models/rf_detr10",dataset_id=None):
     #changes to rf-detr: coco.py line 316 res.dataset['info'] = {}
     batch_size = 16
     grad_accum_steps = 1
@@ -18,7 +19,7 @@ def train_rf_detr(model_size='base',epochs=20,dataset_name="/mnt/fish-detection-
         model = RFDETRLarge()
     
     task = clearml.Task.init(project_name="RF-DETR_Benthic", task_name=f"Train RF-DETR {model_size} model",output_uri=output_uri)
-    dataset = clearml.Dataset.get(dataset_name=dataset_name)
+    dataset = clearml.Dataset.get(dataset_id=dataset_id)
     dataset_path = dataset.get_local_copy()
     model.train(
         dataset_dir=dataset_path,
@@ -38,8 +39,9 @@ def train_rf_detr(model_size='base',epochs=20,dataset_name="/mnt/fish-detection-
 if __name__ == "__main__":
     argparse = argparse.ArgumentParser(description='Train RF-DETR model')
     argparse.add_argument('--model_size',type=str,help="Model size: base, small, medium, large",default='base')
-    argparse.add_argument('--epochs',type=int,help="Number of training epochs",default=20)
-    argparse.add_argument('--dataset_name',type=str,help="Path to dataset name",default="/mnt/fish-detection-coco-dataset/benthic_f_small/rf_data")
-    argparse.add_argument('--output_uri',type=str,help="Path to output directory for saving model checkpoints",default="/mnt/fish-detection-coco-dataset/benthic_f_small/models/rf_detr10")
+    argparse.add_argument('--epochs',type=int,help="Number of training epochs",default=5)
+    argparse.add_argument('--dataset_name',type=str,help="Path to dataset name",default=("RF-DETR_Benthic","benthic_s_comb","1.0.1"))
+    argparse.add_argument('--dataset_id',type=str,help="Dataset ID",default="e6522f92a45f4cbb89095f22b81548c9")
+    argparse.add_argument('--output_uri',type=str,help="Path to output directory for saving model checkpoints",default="/mnt/fish-detection-coco-dataset/")
     args = argparse.parse_args()
-    train_rf_detr(model_size=args.model_size,epochs=args.epochs,dataset_name=args.dataset_name,output_uri=args.output_uri)    
+    train_rf_detr(model_size=args.model_size,epochs=args.epochs,dataset_name=args.dataset_name,output_uri=args.output_uri,dataset_id=args.dataset_id)    
